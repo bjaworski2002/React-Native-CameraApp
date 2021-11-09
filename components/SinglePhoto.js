088fb7044a6ae130a4cf7968c01881cc46660a09
bjaworski2002 <59733034+bjaworski2002@users.noreply.github.com>
2021-11-09 17:48:01 +0100

    Sharing

--- components/SinglePhoto.js
+++ components/SinglePhoto.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import {Text, TouchableOpacity, View, Image, Dimensions, Share } from 'react-native';
+import { Text, TouchableOpacity, View, Image, Dimensions, Share } from 'react-native';
 import * as MediaLibrary from "expo-media-library";
-
-export default function SinglePhoto(props){
+import * as Sharing from "expo-sharing"
+export default function SinglePhoto(props) {
 
     const w = Dimensions.get("window").width
     const h = Dimensions.get("window").height
-
+    const shareHandle = (asset) => {
+        Sharing.isAvailableAsync() ? Sharing.shareAsync(asset.uri) : alert("Niestety nie możesz udostępniać na tym urządzeniu!")
+    }
     const deleteHandle = async (asset) => {
         try {
             await MediaLibrary.deleteAssetsAsync(asset)
             await props.route.params.onDelete(asset)
             await props.navigation.pop()
-        } catch (e){
+        } catch (e) {
             alert("Musisz wyrazić zgodę na usunięcie zdjęcia!")
         }
     }
 
-    return(<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Image style={{flex: 4, width: w * 0.7, height: h * 0.7, resizeMode: 'contain'}}
-               source={{
-                   uri: props.route.params.item.uri
-               }}
+    return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Image style={{ flex: 4, width: w * 0.7, height: h * 0.7, resizeMode: 'contain' }}
+            source={{
+                uri: props.route.params.item.uri
+            }}
         />
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: "space-around"}}>
-            <TouchableOpacity onPress={() => /* TO DO */console.log("123")}><Text style={{margin: w * 0.04, fontSize: w * 0.08}}>SHARE</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => deleteHandle(props.route.params.item)}><Text style={{margin: w * 0.04, fontSize: w * 0.08}}>DELETE</Text></TouchableOpacity>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: "space-around" }}>
+            <TouchableOpacity onPress={() => shareHandle(props.route.params.item)}><Text style={{ margin: w * 0.04, fontSize: w * 0.08 }}>SHARE</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => deleteHandle(props.route.params.item)}><Text style={{ margin: w * 0.04, fontSize: w * 0.08 }}>DELETE</Text></TouchableOpacity>
         </View>
     </View>)
 }
